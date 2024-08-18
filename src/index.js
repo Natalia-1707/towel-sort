e445251f9243e4f006d849042876aab48fd77ccc
Natalia-1707 <nataliatsepoukhowa@gmail.com>
2024-08-18 17:59:03 +0300

    feat: made tower

--- src/index.js
+++ src/index.js
@@ -2,5 +2,20 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!Array.isArray(matrix)) {
+    return [];
+  }
+  let final = [];
+  if (matrix.length === 0) {
+    return matrix;
+  }
+  for (let i = 0; i < matrix.length - 1; i+=2) {
+      final.push(matrix[i], matrix[i + 1].reverse());
+  }
+  if (matrix.length % 2 === 0) {
+    return final.flat(matrix.length);
+  }
+  if (matrix.length % 2 !== 0) {
+     return final.concat(matrix[matrix.length - 1]).flat(matrix.length);
+  }
 }
